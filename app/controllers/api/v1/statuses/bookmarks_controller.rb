a5686aadaf29a4547344dad5a79ba7bcb07fe3d0
Duarte Serrano <duarte.f.m.serrano@tecnico.ulisboa.pt>
2026-05-03 17:43:29 +0000

    Feature: Add bookmark folders
    This feature implements bookmark folders, a way for users to organize
    their bookmarked posts into different folders, we built upon the
    existing bookmark functionality and tried to create something visually
    familiar to the user and completely optional, with no impact on users
    who choose not to use it.
    
    Its possible to create folders, edit their names and delete them.
    
    Co-authored-by: David Bento <david.p.bento@tecnico.ulisboa.pt>

--- app/controllers/api/v1/statuses/bookmarks_controller.rb
+++ app/controllers/api/v1/statuses/bookmarks_controller.rb
@@ -6,7 +6,8 @@ class Api::V1::Statuses::BookmarksController < Api::V1::Statuses::BaseController
   skip_before_action :set_status, only: [:destroy]
 
   def create
-    current_account.bookmarks.find_or_create_by!(account: current_account, status: @status)
+    bookmark = current_account.bookmarks.find_or_initialize_by(status: @status)
+    bookmark.update!(bookmark_params)
     render json: @status, serializer: REST::StatusSerializer
   end
 
@@ -25,5 +26,11 @@ class Api::V1::Statuses::BookmarksController < Api::V1::Statuses::BaseController
     render json: @status, serializer: REST::StatusSerializer, relationships: StatusRelationshipsPresenter.new([@status], current_account.id, bookmarks_map: { @status.id => false })
   rescue ActiveRecord::RecordNotFound, Mastodon::NotPermittedError
     not_found
+  end
+
+  private
+
+  def bookmark_params
+    params.permit(:folder_id)
   end
 end
